d3498ebb8fb553578da9eb45102df7d2a24fbacb
tianmingjian <tianmingjian@work.com>
2024-11-26 17:25:13 +0800

    修复vitepress打包报错

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -72,6 +72,20 @@ export default defineConfig({
       "alias": {
         "@": resolve("./src"),
       }
-    }
+    },
+    ssr: {
+      noExternal: ['element-plus', "react", "react-dom"],
+    },
+    build: {
+      chunkSizeWarningLimit: 100,
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+            return;
+          }
+          warn(warning);
+        },
+      },
+    },
   }
 });
